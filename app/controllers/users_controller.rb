8c98d5360496203def6039e7f27141b6c8145137
lijunhao30246@sina.com <lijunhao30246@sina.com>
2017-02-13 15:28:23 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
-  def new
-  end
-
+  #展示某个用户
   def show
     @user = User.find(params[:id])
   end
+
+  #获取用户注册页面
+  def new
+    @user = User.new
+  end
+
+  #用户填写表单注册账号
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #处理注册成功的情况
+      flash[:success]="Welcome to the Sample App!"
+      redirect_to user_url @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
